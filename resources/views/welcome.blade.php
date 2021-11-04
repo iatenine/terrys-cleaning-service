921d6f1577115484ec562a9df97b0bb492f50421
Jack Linhart <iatenine@gmail.com>
2021-11-03 22:50:31 -0600

    Added bootstrap CSS to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Terry's Cleaning Service</title>
 
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+
 
         <!-- Styles -->
         <style>
@@ -18,8 +22,13 @@
         <style>
             body {
                 font-family: 'Nunito', sans-serif;
+                background-color: #22438d;
             }
         </style>
     </head>
-    <div id="example"></div><script src="{{ asset('js/app.js') }}"></script>
+    <body>
+    <div id="example"></div>
+    </body>
+
+    <script src="{{ asset('js/app.js') }}"></script>
 </html>
